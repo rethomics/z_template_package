bbfe8d4b06614eb48b16bb86b0e15fb3fa9588d5
Quentin Geissmann <qgeissmann@gmail.com>
2018-05-04 18:54:57 +0100

    debug

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,4 +1,6 @@
 set -e
+pwd
+ls
 if [[ "${R_DEVEL}" ]]; then R --silent -e "devtools::install_github('Rdatatable/data.table')"; fi
 R CMD build .
 R CMD check *tar.gz
---
+++ make_new_package.sh
@@ -0,0 +1,10 @@
+#To initiate a new package 
+TEMPLATE=z_template_package
+# all these files are shared between all packages:
+template_files=".gitignore  Makefile .publish_doc.sh .Rbuildignore README.Rmd .travis_after_success.sh .travis_script.sh"
+#git submodule  add https://github.com/rethomics/$TEMPLATE.git
+for t in $template_files; do ln -vsf $TEMPLATE/$t $t;done
+cp $TEMPLATE/.travis.yml .travis.yml
+
+
+
