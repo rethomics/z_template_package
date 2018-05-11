12bce9ce60827633311f59878a34e975135276e8
Quentin Geissmann <qgeissmann@gmail.com>
2018-05-11 11:35:50 +0100

    Update .travis_check.sh

--- .travis_check.sh
+++ .travis_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $R_DEVEL
-if [[ "${R_DEVEL}" ]]; then R --silent -e "devtools::install_github('Rdatatable/data.table')"; fi
+#if [[ "${R_DEVEL}" ]]; then R --silent -e "devtools::install_github('Rdatatable/data.table')"; fi
 R CMD build .
 R CMD check *tar.gz
